36a784c90bd724bebcd4435157b8079695d900eb
PHILIPPE PESCATORE 2 <escortmac@pc8.home>
2015-03-10 09:21:30 +0100

    première révision du projet

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var etiquetteDuMessage: UILabel!
+    @IBOutlet weak var champSaisieDuNom: UITextField!
+    @IBOutlet weak var champSaisieDuMessage: UITextField!
+    @IBOutlet weak var boutonCourrier: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func envoyerLeCourrielAuBoutonPresse(sender: UIButton) {
+        //modifier les Attributs de l'Etiquette
+        //afficher l'étiquette du message dans la Vue
+        etiquetteDuMessage.hidden = false
+        //assigner le texte du message au texte de l'étiquette
+        etiquetteDuMessage.text = champSaisieDuMessage.text
+        //assigner le rouge comme Couleur du texte
+        etiquetteDuMessage.textColor = UIColor.redColor()
+        
+        //RAZ du champ message
+        champSaisieDuMessage.text = ""
+        //escamoter le clavier
+        champSaisieDuMessage.resignFirstResponder()
+        
+        //modifier le titre du Bouton
+        boutonCourrier.setTitle("Courriel envoyé !", forState: UIControlState.Normal)
+    }
 
 }
 
